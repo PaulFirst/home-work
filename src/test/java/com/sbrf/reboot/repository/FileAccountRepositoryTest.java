d975b509c1a00d6c56b526ade87c06026cf8cd81
Pavel Maslov <pavelm320@gmail.com>
2022-01-10 12:34:50 +0700

    Изменен один тест и добавлены 3 новых

--- src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/FileAccountRepositoryTest.java
@@ -3,7 +3,6 @@ package com.sbrf.reboot.repository;
 import com.sbrf.reboot.repository.impl.FileAccountRepository;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,7 +15,7 @@ class FileAccountRepositoryTest {
     AccountRepository accountRepository;
 
     @Test
-    void onlyPersonalAccounts() throws IOException {
+    void onlyPersonalAccounts() {
         String filePath = "src/main/resources/Accounts.txt";
         accountRepository = new FileAccountRepository(filePath);
 
@@ -38,10 +37,57 @@ class FileAccountRepositoryTest {
 
         String filePath = "somePath";
 
-        accountRepository = new FileAccountRepository(filePath);
-
-        assertThrows(FileNotFoundException.class, () -> accountRepository.getAllAccountsByClientId(clientId));
+        assertThrows(RuntimeException.class, () -> accountRepository = new FileAccountRepository(filePath));
     }
 
+    @Test
+    void failGetNonExistentClient() {
+        String filePath = "src/main/resources/Accounts.txt";
+        accountRepository = new FileAccountRepository(filePath);
+
+        long clientId = 44L;
+
+        assertThrows(RuntimeException.class, () -> accountRepository.getAllAccountsByClientId(clientId));
+    }
+
+    @Test
+    void accountsCanBeUpdated() {
+        String filePath = "src/main/resources/Accounts.txt";
+        accountRepository = new FileAccountRepository(filePath);
+
+        long clientId = 1L;
+
+        accountRepository.updateAccountById(clientId, 222L, 524L);
+        Set<Long> actualAccounts = accountRepository.getAllAccountsByClientId(clientId);
+
+        Set<Long> expectedFirst = new HashSet<Long>() {{
+            add(111L);
+            add(524L);
+            add(333L);
+        }};
+
+        actualAccounts.forEach(e -> assertTrue(expectedFirst.contains(e)));
+
+        accountRepository.updateAccountById(clientId, 524L, 222L);
+        actualAccounts = accountRepository.getAllAccountsByClientId(clientId);
+
+        Set<Long> expectedSecond = new HashSet<Long>() {{
+            add(111L);
+            add(222L);
+            add(333L);
+        }};
+
+        actualAccounts.forEach(e -> assertTrue(expectedSecond.contains(e)));
+    }
+
+    @Test
+    void failUpdateNotExistentAccount() {
+        String filePath = "src/main/resources/Accounts.txt";
+        accountRepository = new FileAccountRepository(filePath);
+
+        long clientId = 1L;
+
+        assertThrows(RuntimeException.class, () -> accountRepository.updateAccountById(clientId, 225L, 444L));
+    }
 
 }
