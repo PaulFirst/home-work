01d1bb1d08c498677d1be441ba78f33853a244ad
Pavel Maslov <pavelm320@gmail.com>
2022-01-18 20:16:42 +0700

    созданы сущности Banknote и Cassette

---
+++ src/main/java/com/sbrf/reboot/atm/Cassette.java
@@ -0,0 +1,16 @@
+package com.sbrf.reboot.atm;
+
+import java.util.ArrayList;
+
+public class Cassette<T> {
+
+    ArrayList<T> banknotes = new ArrayList<>();
+
+    public Cassette(ArrayList<T> banknotes) {
+        this.banknotes.addAll(banknotes);
+    }
+
+    public int getCountBanknotes() {
+        return banknotes.size();
+    }
+}
