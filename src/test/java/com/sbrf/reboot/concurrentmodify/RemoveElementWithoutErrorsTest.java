c8a2f640aa7e59d11aa6004e8ed98525b6ada52b
Pavel Maslov <pavelm320@gmail.com>
2022-01-28 19:38:28 +0700

    добавлены тесты с вариантами успешного удаления элемента коллекции при итерировании

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -3,9 +3,12 @@ package com.sbrf.reboot.concurrentmodify;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ConcurrentModificationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -30,8 +33,51 @@ public class RemoveElementWithoutErrorsTest {
     }
 
     @Test
-    public void successRemoveElementWithoutErrors() {
+    public void successRemoveElementWithoutErrorsVar1() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
 
+        list.removeIf(num -> num == 3);
+
+        assertEquals(Arrays.asList(1, 2, 4), list);
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsVar2() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list = list.stream()
+                .filter(num -> num != 3)
+                .collect(Collectors.toList());
+
+        assertEquals(Arrays.asList(1, 2, 4), list);
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsVar3() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        List<Integer> excludedList = new ArrayList<>();
+
+        for(Integer num : list) {
+            if (num == 3) {
+                excludedList.add(num);
+            }
+        }
+        list.removeAll(excludedList);
+
+        assertEquals(Arrays.asList(1, 2, 4), list);
     }
 
 }
