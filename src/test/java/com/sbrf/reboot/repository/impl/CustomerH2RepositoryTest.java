fc7bc7d72766f25734d07d3c1bf0e075310b4473
Pavel Maslov <pavelm320@gmail.com>
2022-02-13 01:11:01 +0700

    добавлены тесты для новых функций обращения к БД

--- src/test/java/com/sbrf/reboot/repository/impl/CustomerH2RepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/impl/CustomerH2RepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CustomerH2RepositoryTest {
@@ -33,5 +34,48 @@ class CustomerH2RepositoryTest {
         boolean mariaCreated = customerRepository.createCustomer("Maria", "maria98@ya.ru");
 
         assertTrue(mariaCreated);
+    }
+
+    @Test
+    void updateCustomer() {
+
+        boolean customerCreated = customerRepository.createCustomer("Bob", "bob12@mail.ru");
+        assertTrue(customerCreated);
+
+        List<Customer> customersFirst = customerRepository.getAll();
+
+        boolean customerUpdated = customerRepository.updateCustomer("John",
+                "john7@mail.ru", "Bob", "bob12@mail.ru");
+        assertTrue(customerUpdated);
+
+        List<Customer> customersSecond = customerRepository.getAll();
+
+        assertTrue(customersFirst.size() == customersSecond.size() &&
+                !customersFirst.containsAll(customersSecond) && !customersSecond.containsAll(customersFirst));
+
+        boolean customerUpdatedBack = customerRepository.updateCustomer("Bob",
+                "bob12@mail.ru", "John", "john7@mail.ru");
+        assertTrue(customerUpdatedBack);
+
+        List<Customer> customersThird = customerRepository.getAll();
+
+        assertTrue(customersFirst.size() == customersThird.size() &&
+                customersFirst.containsAll(customersThird) && customersThird.containsAll(customersFirst));
+
+    }
+
+    @Test
+    void deleteCustomer() {
+        boolean customerCreated = customerRepository.createCustomer("temporal", "temp@mail.ru");
+        assertTrue(customerCreated);
+
+        List<Customer> customersFirst = customerRepository.getAll();
+
+        boolean customerDeleted = customerRepository.deleteCustomer("temporal", "temp@mail.ru");
+        assertTrue(customerDeleted);
+
+        List<Customer> customersLast = customerRepository.getAll();
+
+        assertEquals(customersFirst.size() - 1, customersLast.size());
     }
 }
