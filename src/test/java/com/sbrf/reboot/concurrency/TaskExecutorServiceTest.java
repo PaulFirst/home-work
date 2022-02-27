0030b67c28922d537819b97caf41e71f45c12f3f
Pavel Maslov <pavelm320@gmail.com>
2022-02-27 14:44:06 +0700

    Скорректирована реализация TaskExecutorService и добавлены тесты

--- src/test/java/com/sbrf/reboot/concurrency/TaskExecutorServiceTest.java
+++ src/test/java/com/sbrf/reboot/concurrency/TaskExecutorServiceTest.java
@@ -19,7 +19,7 @@ public class TaskExecutorServiceTest {
         Task task = Mockito.mock(Task.class);
         CountDownLatch latch = new CountDownLatch(2);
 
-        TaskExecutorService taskExecutorService = new TaskExecutorService(2);
+        TaskExecutorService taskExecutorService = new TaskExecutorService(2, 0);
 
         doAnswer((e -> {
             latch.countDown();
@@ -32,6 +32,78 @@ public class TaskExecutorServiceTest {
 
         assertEquals(0, latch.getCount());
         verify(task, times(2)).run();
+
+        taskExecutorService.shutdown();
+    }
+
+    @Test
+    public void successRunNoMultithreading() throws InterruptedException {
+
+        Task task = Mockito.mock(Task.class);
+        Task taskIO = Mockito.mock(Task.class);
+
+        CountDownLatch latch = new CountDownLatch(0);
+        CountDownLatch latchIO = new CountDownLatch(0);
+
+        TaskExecutorService taskExecutorService = new TaskExecutorService(-3, 0);
+
+        doAnswer((e -> {
+            latch.countDown();
+            return null;
+        })).when(task).run();
+
+        doAnswer((e -> {
+            latchIO.countDown();
+            return null;
+        })).when(taskIO).run();
+
+        taskExecutorService.execute(task);
+        taskExecutorService.executeIO(taskIO);
+
+        latch.await();
+        latchIO.await();
+
+        assertEquals(0, latch.getCount());
+        verify(task, times(0)).run();
+
+        assertEquals(0, latchIO.getCount());
+        verify(taskIO, times(0)).run();
+
+        taskExecutorService.shutdown();
+    }
+
+    @Test
+    public void successRunBothMultithreading() throws InterruptedException {
+
+        Task task = Mockito.mock(Task.class);
+        Task taskIO = Mockito.mock(Task.class);
+
+        CountDownLatch latch = new CountDownLatch(4);
+        CountDownLatch latchIO = new CountDownLatch(3);
+
+        TaskExecutorService taskExecutorService = new TaskExecutorService(4, 3);
+
+        doAnswer((e -> {
+            latch.countDown();
+            return null;
+        })).when(task).run();
+
+        doAnswer((e -> {
+            latchIO.countDown();
+            return null;
+        })).when(taskIO).run();
+
+        taskExecutorService.execute(task);
+        taskExecutorService.executeIO(taskIO);
+
+        latch.await();
+        latchIO.await();
+
+        assertEquals(0, latch.getCount());
+        verify(task, times(4)).run();
+
+        assertEquals(0, latchIO.getCount());
+        verify(taskIO, times(3)).run();
 
         taskExecutorService.shutdown();
     }
